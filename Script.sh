00e4f2ab306048763145096a52b951c77f5662cc
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-08 02:18:04 +0500

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -16,7 +16,7 @@ echo -e "\t\t\t\t\t    #####  ######  ####   ####  #    # ###### #       #    # 
 
 # User is going to enter the length of the password.
 echo -n -e "\n\n\n\nPlease Enter the length of the password \n-> "
-read Password_Lenght
+read Password_Length
 
 # The user will be given 5 different option of passwords to select from. 
 for P in $(seq 1 5);
@@ -25,5 +25,9 @@ do
     openssl rand -base64 48 | cut -c1-$Password_Length # base64 has a limited character set.{Max limit is 48} and here all 48 characters are used.
     # The encryption Algorithm used is {base64} 
     # Openssl will generate random base64 characters 
+    # cut is used to extract sections from each line of input or file.
+    # -c1-$Password_Length 
+        # It takes the output from the previous command (openssl rand -base64 48)
+        # and cuts it from the 1st character to the length specified by the variable $Password_Length.
 done
 
