743d9134e4800db003284e341e033e225e02b3b4
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-08 01:54:37 +0500

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -5,17 +5,17 @@ clear # clear screen command
 #  Password Generator ASCII Art
 echo -e "\n\n\n\n"
 echo ""
-echo -e "\t\t\t\t\t\t\t   #######                                                      ######                                                        "
-echo -e "\t\t\t\t\t\t\t   #     #   ##    ####   ####  #    #  ####  #####  #####     #     # ###### #    # ###### #####    ##   #####  ####  ###### "
-echo -e "\t\t\t\t\t\t\t   #     #  #  #  #      #      #    # #    # #    # #    #    #       #      ##   # #      #    #  #  #    #   #    # #    # "
-echo -e "\t\t\t\t\t\t\t   ######  #    #  ####   ####  #    # #    # #    # #    #    #  #### #####  # #  # #####  #    # #    #   #   #    # #    # "
-echo -e "\t\t\t\t\t\t\t   #       ######      #      # # ## # #    # #####  #    #    #     # #      #  # # #      #####  ######   #   #    # #####  "
-echo -e "\t\t\t\t\t\t\t   #       #    # #    # #    # ##  ## #    # #   #  #    #    #     # #      #   ## #      #   #  #    #   #   #    # #   #  "
-echo -e "\t\t\t\t\t\t\t   #       #    #  ####   ####  #    #  ####  #    # #####      #####  ###### #    # ###### #    # #    #   #    ####  #    # "
+echo -e "\t\t\t\t\t    #####                                     ######                             ######                 "
+echo -e "\t\t\t\t\t   #     # ######  ####  #    # #####  ###### #     #   ##    ####   ####        #     # #####   ####   "
+echo -e "\t\t\t\t\t   #       #      #    # #    # #    # #      #     #  #  #  #      #            #     # #    # #    #  "
+echo -e "\t\t\t\t\t    #####  #####  #      #    # #    # #####  ######  #    #  ####   ####  ##### ######  #    # #    #  "
+echo -e "\t\t\t\t\t         # #      #      #    # #####  #      #       ######      #      #       #       #####  #    #  "
+echo -e "\t\t\t\t\t   #     # #      #    # #    # #   #  #      #       #    # #    # #    #       #       #   #  #    #  "
+echo -e "\t\t\t\t\t    #####  ######  ####   ####  #    # ###### #       #    #  ####   ####        #       #    #  ####  "
 
 
 # User is going to enter the length of the password.
-echo -n -e "Please Enter the length of the password \n-> "
+echo -n -e "\n\n\n\nPlease Enter the length of the password \n-> "
 read Password_Lenght
 
 for P in $(seq 1);
