02ade5b8fc561ddd29313132462380dd0c476dc6
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-02-08 20:49:50 +0500

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -22,26 +22,15 @@ sleep 0.5
 
 
 # User is going to enter the length of the password.
-echo -n -e "\n\n\n\nPlease Enter the length of the password \n-> "
+echo -n -e "\n\n\n\n \t\t\t\t\tPlease Enter the length of the password \n\t\t\t\t\t-> "
 read Password_Length
 
-# Check if the input is valid 
-if ! [[$Password_Length =~ ^[0-9]+$]];
-    # {=~} is the regex matching operator.
-    # {^[0-9]+$} s a regular expression pattern:
-        # (^) asserts the start of the line.
-        # ([0-9]+) matches one or more digits (0-9).
-        # ($) asserts the end of the line.
-    # {!} negates the condition.
-then 
-    echo "Error: Please enter a valid number."
-    exit 1
-fi
+echo -e "\n\n"
 
 # The user will be given 5 different option of passwords to select from. 
 for P in $(seq 1 5);
 do
-    echo -n "$P.} "
+    echo -e -n "\t\t\t\t\t$P.} "
     openssl rand -base64 48 | cut -c1-$Password_Length # base64 has a limited character set.{Max limit is 48} and here all 48 characters are used.
     # The encryption Algorithm used is {base64} 
     # Openssl will generate random base64 characters 
@@ -50,4 +39,3 @@ do
         # It takes the output from the previous command (openssl rand -base64 48)
         # and cuts it from the 1st character to the length specified by the variable $Password_Length.
 done
-
